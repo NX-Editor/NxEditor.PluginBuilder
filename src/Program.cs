fa2a621de8c4a03f83210a85a79d56ff2e5a8f51
ArchLeaders <archparliament@gmail.com>
2023-06-29 00:27:33 -0700

    Impl builder script

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string current = Directory.GetCurrentDirectory();
+string name = Path.GetFileName(current);
+
+Console.WriteLine($"Renaming project files to '{name}'. . .");
+
+ProcessFiles(current);
+ProcessFiles(Path.Combine(current, "src"));
+
+void ProcessFiles(string dir)
+{
+    foreach (var file in Directory.EnumerateFiles(dir, "*.*")) {
+        string newFile = Path.Combine(dir, Path.GetRelativePath(dir, file).Replace("NxEditor.PluginTemplate", name));
+
+        string value = File.ReadAllText(file);
+        value = value.Replace("NxEditor.PluginTemplate", name);
+
+        File.Delete(file);
+        File.WriteAllText(newFile, value);
+    }
+}
